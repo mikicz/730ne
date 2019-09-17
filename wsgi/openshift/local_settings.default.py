27ac075d74a626f80a8197a56dbfadd4b85371ac
Mikuláš Poul <mikulaspoul@gmail.com>
2019-09-17 15:51:25 +0100

    Added default local_settings.py, added missing migrations for Signature

---
+++ wsgi/openshift/local_settings.default.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '730ne',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+ALLOWED_HOSTS = ["*"]
+DEBUG = True
+
